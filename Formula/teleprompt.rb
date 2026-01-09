83407e28c108c44a1c8d7cd7b985e273152261e8
Roy Lee <roylee17@gmail.com>
2026-01-09 13:08:20 -0500

    teleprompt 0.3.0

--- Formula/teleprompt.rb
+++ Formula/teleprompt.rb
@@ -1,13 +1,13 @@
 class Teleprompt < Formula
   desc "CLI tool for managing and syncing Claude Code configurations across computers"
   homepage "https://github.com/roylee17/teleprompt"
-  version "0.2.2"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/roylee17/teleprompt/releases/download/v0.2.2/release-darwin-arm64.tar.gz"
-      sha256 "ca19e49ddce38d9bcd412adf13a768b92e94534e7fe54954a4cbb690f60d3cc1"
+      url "https://github.com/roylee17/teleprompt/releases/download/v0.3.0/release-darwin-arm64.tar.gz"
+      sha256 "9c47a6ad3788a40d380ec5d48a5ed7badc6c7403f7ce5f1e34fe9c39a8bc919d"
 
       def install
         bin.install "teleprompt-darwin-arm64" => "teleprompt"
@@ -15,8 +15,8 @@ class Teleprompt < Formula
     end
 
     on_intel do
-      url "https://github.com/roylee17/teleprompt/releases/download/v0.2.2/release-darwin-amd64.tar.gz"
-      sha256 "b1f90bae7e11973edb8f8d1a84fb058e751ef1969d76ee2a4eb793ef190fc980"
+      url "https://github.com/roylee17/teleprompt/releases/download/v0.3.0/release-darwin-amd64.tar.gz"
+      sha256 "ae8709858668c0c92e14308f5f10c1b7c21cc751260c25b12e5f59c95d116673"
 
       def install
         bin.install "teleprompt-darwin-amd64" => "teleprompt"
@@ -26,8 +26,8 @@ class Teleprompt < Formula
 
   on_linux do
     on_intel do
-      url "https://github.com/roylee17/teleprompt/releases/download/v0.2.2/release-linux-amd64.tar.gz"
-      sha256 "829ae28fdc1776e0b3bd83ba345dbca4106745a9f7d74c70e862696de40dc46d"
+      url "https://github.com/roylee17/teleprompt/releases/download/v0.3.0/release-linux-amd64.tar.gz"
+      sha256 "7800abf0eb97f214563c32e2d0016e53dbbdcba022de9483f4a794df2b6d15c7"
 
       def install
         bin.install "teleprompt-linux-amd64" => "teleprompt"
@@ -35,8 +35,8 @@ class Teleprompt < Formula
     end
 
     on_arm do
-      url "https://github.com/roylee17/teleprompt/releases/download/v0.2.2/release-linux-arm64.tar.gz"
-      sha256 "f178ed710d027c62318542603202010dfab16760b8965c815e61da5ed6e5d0c0"
+      url "https://github.com/roylee17/teleprompt/releases/download/v0.3.0/release-linux-arm64.tar.gz"
+      sha256 "c01712f761527275542b4a2e469e8343805faf4e221105b817a1649f8e7cc577"
 
       def install
         bin.install "teleprompt-linux-arm64" => "teleprompt"
